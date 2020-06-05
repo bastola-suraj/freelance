c22fb9f9a8c3119ab01d6c0697cb1357d3f86337
Suraj Bastola <bastolasuraj@outlook.com>
2020-06-05 16:09:44 +0545

    DB Changes

--- db-conn.php
+++ db-conn.php
@@ -1,4 +1,4 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;dbname=freelance_db','root','root');
+$pdo=new PDO('mysql:host=localhost;dbname=freelance_db','sbastola','Ynirwiv6mk..12');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
--- db.sql
+++ db.sql
@@ -1,29 +1,29 @@
 create table institute(
-id integer auto_increment primary key,
+id int auto_increment primary key,
 name varchar(100)
 );
 
 create table company(
-id integer auto_increment primary key,
+id int auto_increment primary key,
 name varchar(100)
 );
 
 create table positions(
-id integer auto_increment primary key,
+id int auto_increment primary key,
 name varchar(100)
 );
 
 create table skills(
-id integer auto_increment primary key,
+id int auto_increment primary key,
 name varchar(100)
 );
 
 create table educations(
-id integer auto_increment primary key,
+id int auto_increment primary key,
 start_year varchar(20),
 end_year varchar(20),
 description text,
-institute_id integer,
+institute_id int,
 constraint edu_institutefk
 foreign key (institute_id) 
 references institute(id)
@@ -31,11 +31,11 @@ on delete cascade on update cascade
 );
 
 create table job_history(
-id integer auto_increment primary key,
+id int auto_increment primary key,
 start_year varchar(20),
 end_year varchar(20),
-position_id integer,
-company_id integer,
+position_id int,
+company_id int,
 description text,
 constraint job_company
 foreign key (company_id)
@@ -48,17 +48,17 @@ on delete cascade on update cascade
 );
 
 create table freelancer(
-id integer auto_increment primary key,
-users_id integer,
-education_id integer,
-jobhistory_id integer,
+id int auto_increment primary key,
+users_id int,
+education_id int,
+jobhistory_id int,
 join_date varchar(20),
-skills_id integer,
+skills_id int,
 description text,
 headline varchar(50),
 constraint freelancer_education
 foreign key (education_id) 
-references education(id)
+references educations(id)
 on delete cascade on update cascade,
 constraint freelancer_jobs
 foreign key (jobhistory_id) 
